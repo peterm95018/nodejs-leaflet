f39a2a9de28ce5e47d84878fcc1ce55d408af903
Peter McMillan <peterm95018@gmail.com>
2015-07-13 16:40:43 -0700

    reset map to point to UCSC

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([37.0020, -122.0600], 15);
 
 		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 			maxZoom: 18,

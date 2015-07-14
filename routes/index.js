150fb8b07b0a8f73cc31b6c48f8eddc0b21c91ab
Peter McMillan <peterm@ucsc.edu>
2015-07-14 15:44:41 -0700

    changed Members to Maps

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Members' });
+  res.render('index', { title: 'Maps' });
 });
 
 module.exports = router;

8fe622d13f403e757c1157700162e956973ab0bd
Peter McMillan <peterm95018@gmail.com>
2015-10-13 13:13:21 -0700

    working SSL web server config. No working passport-ldapauth

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
-
 var passport = require('passport');
-// var LocalStrategy = require('passport-local').Strategy;
-//var LdapStrategy = require('passport-ldapauth');
+var LdapStrategy = require('passport-ldapauth');
 
-// try using ldapauth-fork instructions
-var connect = require('connect');
-var LdapAuth = require('ldapauth-fork');
+var OPTS = {
+  server: {
+    url: 'ldaps://ldap-99.soe.ucsc.edu:636',
+    bindDn: 'cn=root',
+    searchBase: 'dc=crm,dc=ucsc,dc=edu',
+    // searchFilter: '(uid={{username}})',
+    // bindCredentials: '{{password}}',
+        searchFilter: '(uid={{username}})',
+    bindCredentials: '{{password}}',
+    //searchAttributes: ['displayName', 'mail']
+  }
+}
+
+passport.use(new LdapStrategy(OPTS));
 
 
 /* GET users listing. */
@@ -32,51 +40,7 @@ router.get('/login', function(req, res, next) {
   });
 });
 
-// Config from a .json or .ini file or whatever.
-var config = {
-  ldap: {
-    url: "ldaps://ldap-99.soe.ucsc.edu:636",
-    bindDn: 'ou=People,dc=crm,dc=ucsc,dc=edu',
-    bindCredentials: "password",
-    searchBase: "ou=users,o=example.com",
-    searchFilter: "(uid={{username}})"
-  }
-};
 
-var ldap = new LdapAuth({
-  url: config.ldap.url,
-  bindDn: config.ldap.bindDn,
-  bindCredentials: config.ldap.bindCredentials,
-  searchBase: config.ldap.searchBase,
-  searchFilter: config.ldap.searchFilter,
-  //log4js: require('log4js'),
-  cache: true
-});
-
-var basicAuthMiddleware = connect.basicAuth(function (username, password, callback) {
-  ldap.authenticate(username, password, function (err, user) {
-    if (err) {
-      console.log("LDAP auth error: %s", err);
-    }
-    callback(err, user)
-  });
-});
-
-
-// var OPTS = {
-// 	server: {
-// 		url: 'ldap://ldap-99.soe.ucsc.edu',
-// 		bindDn: 'ou=People,dc=crm,dc=ucsc,dc=edu',
-// 		bindCredentials: '{{password}}',
-// 		searchBase: 'ou=People,dc=crm,dc=ucsc,dc=edu',
-// 		searchFilter: '(uid={{username}})'
-// 	}
-// };
-
-
-
-//passport.use(new LdapStrategy(OPTS));
-//passport.use(new LdapStrategy(OPTS));
 
 
 // from http://code.runnable.com/VOd1LNZyrqxYnQES/nodejs-passport-ldapauth-express-test-for-node-js-and-hello-world
@@ -95,14 +59,14 @@ var basicAuthMiddleware = connect.basicAuth(function (username, password, callba
 
 
 
-// router.post('/login', passport.authenticate('ldapauth',
-// 	{session: false}), 
-// 	function(req, res) {
-// 		console.log('Authentication Successful');
-// 		req.flash('success', 'You are logged in');
-// 		res.redirect('/');
+router.post('/login', passport.authenticate('ldapauth', {session: false}), 
+	function(req, res) {
+    res.send({status: 'ok'});
+		console.log('Authentication Successful');
+		req.flash('success', 'You are logged in');
+		res.redirect('/');
 
-// 	});
+	});
 
 
 

f40b234d9848dc57b0d7b058291eb10039330505
Peter McMillan <peterm95018@gmail.com>
2016-09-13 14:33:50 -0700

    fixed multer lines when app crashed at startup

--- app.js
+++ app.js
@@ -27,10 +27,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// handle file uploads
-app.use(multer({
-  dest: './uploads'
-}));
+
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));

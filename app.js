b936e9084010b3ae291ddf362fa47ccb062c57b2
Peter McMillan <peterm95018@gmail.com>
2016-10-04 14:45:54 -0700

    fixed the multer block

--- app.js
+++ app.js
@@ -27,10 +27,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // handle file uploads
-app.use(multer({
+var multer = require('multer');
+var upload = multer({ 
   dest: './uploads'
-}));
+  });
+
+// app.use(multer({
+//   dest: './uploads'
+// }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));

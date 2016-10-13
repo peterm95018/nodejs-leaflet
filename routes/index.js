ecdcec6799b10f09f151941ff4dcf61c5a19b260
Peter McMillan <peterm95018@gmail.com>
2016-10-13 12:35:59 -0700

    readding code to display LDAP object to profile template. Big learning is that we set the res.locals in the index prior to rendering the page on the route.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
 
+router.get('/profile', ensureAuthenticated, function(req, res, next) {
+	res.locals.testUser = req.session.passport.user;
+  res.render('profile', { title: 'Profile' });
+});
+
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
